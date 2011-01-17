da69ce714d5c79aab8f9270f128f4394b0772b0d
Supriyo Sinha <supriyosinha@myhost.local>
2011-01-16 21:38:45 -0500

    deleted map.js

--- mapreduce/map.js
+++
@@ -1,9 +0,0 @@
-function map() {
-    var words = this.title.match(/\w+/g);
-    
-    if(words == null) return;
-    
-    for(var i = 0; i < words.length; i++){
-        emit(words[i], {count:1, score: this.score});
-    }
-}
