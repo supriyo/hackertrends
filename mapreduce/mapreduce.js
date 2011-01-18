c068acfaff732fd6ee1b0ac87239f5fc90e20dcc
Supriyo Sinha <supriyosinha@myhost.local>
2011-01-17 20:00:26 -0500

    Corrected average_comments formula

--- mapreduce/mapreduce.js
+++ mapreduce/mapreduce.js
@@ -1,7 +1,6 @@
 exports.mapReduce = function(db, command, processResults, resultCollection){
     db.open(function(err, db){
         db.executeDbCommand(command, function(err, dbres){
-            console.log(dbres)
            processResults(db, dbres.documents[0].result, resultCollection)
         });
     });   
