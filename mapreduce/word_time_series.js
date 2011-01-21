6f6e4eb286eae1b4b08e84ca72c9318adc94ad05
Supriyo Sinha <supriyosinha@myhost.local>
2011-01-21 12:44:44 -0500

    Fixed the word_time_series stuff

--- mapreduce/word_time_series.js
+++ mapreduce/word_time_series.js
@@ -14,20 +14,33 @@
         if(words == null) return;
         
         for(var i = 0; i < words.length; i++){
-            emit({date: new Date(Date.parse(this.age)), word: words[i]}, {count:1});
+            emit({date: new Date(Date.parse(this.age)), 
+                    word: words[i]}, 
+                    {
+                        count: 1,
+                        score: this.score,
+                        num_comments: this.num_comments,
+                    }
+                );
         }
     }
 
     function reduce(key, values) {
         var count = 0;
-    
+        var score = 0;
+        var num_comments = 0;
         values.forEach(function(doc){
             count += doc.count;
-        })
+            score += doc.score;
+            num_comments += doc.num_comments;
+        });
         
-        return {count:count}
+        return {
+            count:count, 
+            score:score/count, 
+            num_comments: num_comments/count,
+        }
     }
-
 
     command = {
             mapreduce: "posts",
@@ -35,9 +48,11 @@
             map: map.toString(),
             reduce: reduce.toString(),
         }
+        
     db.open(function(err, db){
         db.executeDbCommand(command, function(err, dbres){
             db.close()
+            console.log("Comlete. DB closed.")
         });
     });
 })();
