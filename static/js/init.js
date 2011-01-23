7a5983cc5221619c94ffd92c503ff90d4f48a50a
Alec Perkins <alecperkins@gmail.com>
2011-01-22 23:15:27 -0500

    JSONP support

--- static/js/init.js
+++ static/js/init.js
@@ -1,5 +1,3 @@
-
-
 var init = function() {
     var timer = new Timer('#refresh-counter');
     data_manager = new DataManager();
@@ -15,66 +13,16 @@ var init = function() {
     $("#refresh").click(refresh);
     
     var tab_callbacks = {
-        'word-graph': function () {}, //refreshWordGraph
+        'word-graph': views.redrawWordGraph,
         'post-graph': views.redrawPostGraphs,
         'post-table': views.redrawPostTable
     }
     TabManager('.tab','.box', tab_callbacks);
     
-    $("#search").click( function () {
-        //TODO: Move this into a graph function
-        var q = $("#q").val();
-        if(q!=''){
-            $.getJSON('/query-time-series/', {q:q}, function (response) {
-                var lines = [];
-                for(var i=0; i < response.length; i++){
-                    var line = [];
-                    for(var j=0; j<response[i].length; j++){
-                        var date = response[i][j]["_id"]["date"];
-                        line.push([Date.parse(date), response[i][j]["value"]["count"]])
-                    }
-                    lines.push(line);
-                }
-                $.plot($("#wordfrequency"), lines, {
-                    series:{
-                      points: {show:true},
-                      lines: {show:true}
-                    },
-                    legend: {
-                        show: true
-                    },
-                    xaxis: {
-                        mode: "time",
-                        ticks: function (range){
-                            var ticks = [];
-                            for(var t=range.min; t<=range.max; t+=1000*60*60*24){
-                                ticks.push(t);
-                            }
-                            return ticks;
-                        },
-                        timeformat: "%b %d, %y"
-                    },
-                    yaxis: {
-                        min: 0,
-                        ticks: function(range) {
-                            var ticks = []
-                            for(var i=0; i< 2*range.max; i++){
-                                ticks.push(i);
-                            }
-                            return ticks;
-                        }
-                    }
-                });
-            });
-        }
-    });
-    
-    $("#q").val("Apple");
+    $("#search").click( views.redrawWordGraph );
+    $("#q").val("HN");
     $("#search").click();
     refresh();
 }
+
 $(document).ready(init);
-
-
-
-
